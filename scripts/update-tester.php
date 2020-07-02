82c670058388a4312924396c3340a423dfc45805
Daniel Bosen <daniel.bosen@burda.com>
2020-07-02 09:56:34 +0200

    Drush 10 compatibility (#19)

--- scripts/update-tester.php
+++ scripts/update-tester.php
@@ -208,9 +208,9 @@ class UpdateTester extends Tasks {
     $drushCmd->dir($absoluteDestinationDocroot);
     $drushCmd->printOutput(FALSE);
     $drushCmd
-      ->arg('status')
+      ->arg('core-status')
       ->option('pipe')
-      ->option('show-passwords');
+      ->option('fields', 'db-password,drupal-version,uri,db-driver,db-hostname,db-port,db-username,db-name,db-status,bootstrap,theme,admin-theme,php-bin,php-conf,php-os,drush-script,drush-version,drush-temp,drush-conf,install-profile,root,site,files,private,temp', '=');
 
     /** @var \Robo\Result $resultStatus */
     $resultStatus = $drushCmd->run();
